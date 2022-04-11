250309efbee0b52ce93152cb3ac4d0e641b6ac27
Emerson Novais <emersonnovais93@gmail.com>
2022-04-10 21:38:30 -0300

    App complete
    Finish the app

--- my-anotations/ViewController.swift
+++ my-anotations/ViewController.swift
@@ -8,10 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let keyCode: String = "annotations"
+    @IBOutlet weak var textFieldAnnotations: UITextView!
+    
+    @IBAction func saveAnnotations(_ sender: Any) {
+        if let text = textFieldAnnotations.text {
+            UserDefaults.standard.set(text, forKey: keyCode)
+            print("salvou")
+        }
+    }
+    
+    func recoveryText() -> String {
+        if let recoveredText = UserDefaults.standard.object(forKey: keyCode) {
+            return recoveredText as! String
+        }
+        return ""
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textFieldAnnotations.text = recoveryText()
+        
     }
 
 
